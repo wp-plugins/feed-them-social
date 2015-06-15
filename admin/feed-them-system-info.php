35bf5295fb24c0bc6298c7659d51f9facce7b98d
slickremix <slickremix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-15 17:32:31 +0000

    System Info Fixes!
    git-svn-id: https://plugins.svn.wordpress.org/feed-them-social/trunk@1181254 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/feed-them-system-info.php
+++ admin/feed-them-system-info.php
@@ -1,198 +1,174 @@
 <?php
-
 function feed_them_system_info_page(){
 ?>
+<div class="fts-help-admin-wrap"> <a class="buy-extensions-btn" href="http://www.slickremix.com/downloads/category/feed-them-social/" target="_blank">
+<?php _e( 'Get Extensions Here!', 'feed-them-social' ); ?>
+</a>
+<h2>
+<?php _e( 'System Info', 'feed-them-social' ); ?>
+</h2>
+<p>
+<?php _e( 'Please click the box below and copy the report. You will need to paste this information along with your question in our', 'feed-them-social' ); ?>
+<a href="http://www.slickremix.com/support-forum/" target="_blank">
+<?php _e( 'Support Forum', 'feed-them-social' ); ?>
+</a>.
+<?php _e( 'Ask your question then paste the copied text below it.  To copy the system info, click below then press Ctrl + C (PC) or Cmd + C (Mac).', 'feed-them-social' ); ?>
+</p>
+<form action="<?php echo esc_url( admin_url( 'admin.php?page=fts-system-info-submenu-page' ) ); ?>" method="post" dir="ltr" >
+<textarea readonly="readonly" onclick="this.focus();this.select()" id="system-info-textarea" name="fts-sysinfo" title="<?php _e( 'To copy the system info, click below then press Ctrl + C (PC) or Cmd + C (Mac).', 'feed-them-social' ); ?>">
+### Begin System Info ###
 
-<div class="fts-help-admin-wrap"> <a class="buy-extensions-btn" href="http://www.slickremix.com/downloads/category/feed-them-social/" target="_blank"><?php _e( 'Get Extensions Here!', 'feed-them-social' ); ?></a>
-  <h2><?php _e( 'System Info', 'feed-them-social' ); ?></h2>
-  <div class="fts-admin-help-wrap">
-    <div class="use-of-plugin"><?php _e( "Can't figure out how to do something and need help? Use our", "feed-them-social" ); ?> <a href="http://www.slickremix.com/support-forum/" target="_blank"><?php _e('Support Forum','feed-them-social')?></a> <?php _e('and someone will respond to your request asap. Usually we will respond the same day, the latest the following day. You may also find some of the existing posts to be helpfull too, so take a look around first. If you do submit a question please','feed-them-social')?> <a href="#" class="fts-debug-report"><?php _e('generate a report','feed-them-social')?></a> <?php _e('and copy the info, ask your question in our','feed-them-social')?> <a href="http://www.slickremix.com/support-forum/" target="_blank"><?php _e('Support Forum','feed-them-social')?></a> <?php _e('then paste the info you just copied. That will help speed things along for sure.','feed-them-social')?></div>
-    </h3>
-    <h3><?php _e( 'Plugin &amp; System Info', 'feed-them-social' ); ?></h3>
-    <p><?php _e( 'Please', 'feed-them-social' ); ?> <a href="#" class="fts-debug-report"><?php _e( 'click here to generate a report', 'feed-them-social' ); ?></a> <?php _e( 'You will need to paste this information along with your question in our', 'feed-them-social' ); ?> <a href="http://www.slickremix.com/support-forum/" target="_blank"><?php _e( 'Support Forum', 'feed-them-social' ); ?></a>. <?php _e( 'Ask your question then paste the copied text below it.', 'feed-them-social' ); ?></p>
-    <textarea id="fts-debug-report" readonly="readonly"></textarea>
-    <table class="wc_status_table widefat" cellspacing="0">
-      <thead>
-        <tr>
-          <th colspan="2"><?php _e( 'Versions', 'feed-them-social' ); ?></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><?php _e('Feed Them Social Plugin version','feed-them-social')?></td>
-          <td><?php echo ftsystem_version(); ?></td>
-        </tr>
-        <tr>
-          <td><?php _e('WordPress version','feed-them-social')?></td>
-          <td><?php if ( is_multisite() ) echo 'WPMU'; else echo 'WP'; ?>
-            <?php echo bloginfo('version'); ?></td>
-        </tr>
-        <tr>
-          <td><?php _e('Installed plugins','feed-them-social')?></td>
-          <td><?php
-             			$active_plugins = (array) get_option( 'active_plugins', array() );
+<?php
+$theme_data = wp_get_theme();
+$theme      = $theme_data->Name . ' ' . $theme_data->Version; ?>
+Multisite:                <?php echo is_multisite() ? 'Yes' . "\n" : 'No' . "\n" ?>
+SITE_URL:                 <?php echo site_url() . "\n"; ?>
+Feed Them Social Version: <?php echo ftsystem_version(). "\n"; ?>
 
-             			if ( is_multisite() )
-							$active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
+-- Wordpress Configuration
 
-						$active_plugins = array_map( 'strtolower', $active_plugins );
+WordPress Version:        <?php echo get_bloginfo( 'version' ) . "\n"; ?>
+Permalink Structure:      <?php echo get_option( 'permalink_structure' ) . "\n"; ?>
+Active Theme:             <?php echo $theme . "\n"; ?>
+PHP Memory Limit:         <?php echo ini_get( 'memory_limit' ) . "\n"; ?>
+WP_DEBUG:                 <?php echo defined( 'WP_DEBUG' ) ? WP_DEBUG ? 'Enabled' . "\n" : 'Disabled' . "\n" : 'Not set' . "\n" ?>
 
-						$wc_plugins = array();
+-- Webserver Configuration
 
-						foreach ( $active_plugins as $plugin ) {
-							//if ( strstr( $plugin, 'feed-them-social' ) ) {
+PHP Version:              <?php echo PHP_VERSION . "\n"; ?>
+MySQL Version:            <?php echo mysql_get_server_info() . "\n"; ?>
+Web Server Info:          <?php echo $_SERVER['SERVER_SOFTWARE'] . "\n"; ?>
 
-								$plugin_data = @get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin );
+-- PHP Configuration:
 
-	    						if ( ! empty( $plugin_data['Name'] ) ) {
+Safe Mode:                <?php echo ini_get( 'safe_mode' ) ? "Yes" : "No\n"; ?>
+Upload Max Size:          <?php echo ini_get( 'upload_max_filesize' ) . "\n"; ?>
+Post Max Size:            <?php echo ini_get( 'post_max_size' ) . "\n"; ?>
+Upload Max Filesize:      <?php echo ini_get( 'upload_max_filesize' ) . "\n"; ?>
+Time Limit:               <?php echo ini_get( 'max_execution_time' ) . "\n"; ?>
+Max Input Vars:           <?php echo ini_get( 'max_input_vars' ) . "\n"; ?>
+Arg Separator:            <?php echo ini_get( 'arg_separator.output' ) . "\n"; ?>
+Allow URL File Open:      <?php echo ( ini_get( 'allow_url_fopen' ) ) ? 'On (' . ini_get( 'display_errors' ) . ')' : 'N/A'; ?><?php echo "\n"; ?>
+Display Erros:            <?php echo ( ini_get( 'display_errors' ) ) ? 'On (' . ini_get( 'display_errors' ) . ')' : 'N/A'; ?><?php echo "\n"; ?>
 
-	    							$wc_plugins[] = $plugin_data['Name'] . ' ' . __('by', 'feed-them-social') . ' ' . $plugin_data['Author'] . ' ' . __('version', 'feed-them-social') . ' ' . $plugin_data['Version'] ;
+-- PHP Extensions:
 
-	    						}
-    						//}
-						}
+FSOCKOPEN:                <?php echo ( function_exists( 'fsockopen' ) ) ? 'Your server supports fsockopen.' : 'Your server does not support fsockopen.'; ?><?php echo "\n"; ?>
+cURL:                     <?php echo ( function_exists( 'curl_init' ) ) ? 'Your server supports cURL.' : 'Your server does not support cURL.'; ?><?php echo "\n"; ?>
 
-						if ( sizeof( $wc_plugins ) == 0 ) echo '-'; else echo '<ul><li>' . implode( ',</li><li>', $wc_plugins ) . '</li></ul>';
+-- Active Plugins:
 
-             		?></td>
-        </tr>
-      </tbody>
-      <thead>
-        <tr>
-          <th colspan="2"><?php _e( 'Server Environment', 'feed-them-social' ); ?></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><?php _e('PHP Version','feed-them-social')?></td>
-          <td><?php
-                    	if ( function_exists( 'phpversion' ) )
-						
-						$phpversion = phpversion();
-						$phpcheck = '5.2.9';
-						
-						if($phpversion > $phpcheck) {
-							 echo phpversion();
-						}
-						else {
-						    echo phpversion();
-							echo '<br/><mark class="no">'; 
-							_e('WARNING: ','feed-them-social');
-							echo '</mark>';
-							 _e('Your version of php must be 5.3 or greater to use this plugin. Please upgrade the php by contacting your host provider. Some host providers will allow you to change this yourself in the hosting control panel too.','feed-them-social');
-						}
-						
-						
-                    ?></td>
-        </tr>
-        <tr>
-          <td><?php _e('Server Software','feed-them-social')?></td>
-          <td><?php
-                    	echo $_SERVER['SERVER_SOFTWARE'];
-                    ?></td>
-        </tr>
-        <tr>
-          <td><?php _e('WP Max Upload Size','feed-them-social'); ?></td>
-          <td><?php
-                    	echo size_format( wp_max_upload_size() );
-                    ?></td>
-        </tr>
-        <tr>
-          <td><?php _e('WP Debug Mode','feed-them-social')?></td>
-          <td><?php if ( defined('WP_DEBUG') && WP_DEBUG ) echo '<mark class="yes">' . __('Yes', 'feed-them-social') . '</mark>'; else echo '<mark class="no">' . __('No', 'feed-them-social') . '</mark>'; ?></td>
-        </tr>
-        <tr>
-          <td><?php _e('fsockopen','feed-them-social')?></td>
-          <td><?php
- if(function_exists('fsockopen')) {
-      _e('fsockopen is ON','feed-them-social');
- }
- else {
-	 _e('fsockopen is not enabled and must be set to ON for our plugin to work properly with all feeds.','feed-them-social');
- }
- ?></td>
-        </tr>
-        
-        <tr>
-        <td><?php _e('cURL','feed-them-social')?></td>
-        <td><?php
-		// Script to test if the CURL extension is installed on this server
+<?php
+$plugins = get_plugins();
+$active_plugins = get_option( 'active_plugins', array() );
 
-// Define function to test
-function _fts_is_curl_installed() {
-    if  (in_array  ('curl', get_loaded_extensions())) {
-        return true;
-    }
-    else {
-        return false;
-    }
+foreach ( $plugins as $plugin_path => $plugin ) {
+	// If the plugin isn't active, don't show it.
+	if ( ! in_array( $plugin_path, $active_plugins ) )
+		continue;
+
+	echo $plugin['Name'] . ': ' . $plugin['Version'] ."\n";
 }
 
-// Ouput text to user based on test
-if (_fts_is_curl_installed()) {
-	 _e('cURL is ','feed-them-social');
-	 echo '<span style="color:blue">';
-	 _e('installed','feed-them-social');
-	 echo '</span> ';
-	 _e('on this server','feed-them-social');
-	 
-	 
-} else {
-	 _e('cURL is NOT ','feed-them-social');
-	 echo '<span style="color:red">';
-	 _e('installed','feed-them-social');
-	 echo '</span> ';
-	 _e('on this server','feed-them-social');
+if ( is_multisite() ) :
+?>
+
+-- Network Active Plugins:
+
+<?php
+$plugins = wp_get_active_network_plugins();
+$active_plugins = get_site_option( 'active_sitewide_plugins', array() );
+
+foreach ( $plugins as $plugin_path ) {
+	$plugin_base = plugin_basename( $plugin_path );
+
+	// If the plugin isn't active, don't show it.
+	if ( ! array_key_exists( $plugin_base, $active_plugins ) )
+		continue;
+
+	$plugin = get_plugin_data( $plugin_path );
+
+	echo $plugin['Name'] . ' :' . $plugin['Version'] ."\n";
 }
-?></td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <!--/fts-admin-help-faqs-wrap--> 
-  
-  <a class="fts-settings-admin-slick-logo" href="http://www.slickremix.com" target="_blank"></a> </div>
-<!--/fts-help-admin-wrap--> 
-<script type="text/javascript">
-		jQuery('a.fts-debug-report').click(function(){
 
-			if ( ! jQuery('#fts-debug-report').val() ) {
+endif;
 
-				// Generate report - user can paste into forum
-				var report = '`';
+$facebookOptions = get_option('fts_facebook_custom_api_token') ? 'yes' : 'no' ;
+$facebookOptionsAppID = get_option('fb_app_ID') ? 'yes' : 'no' ;
+$twitterOptions1 = get_option('fts_twitter_custom_consumer_key') ? 'yes' : 'no' ;
+$twitterOptions2 = get_option('fts_twitter_custom_consumer_secret') ? 'yes' : 'no' ;
+$twitterOptions3 = get_option('fts_twitter_custom_access_token') ? 'yes' : 'no' ;
+$twitterOptions4 = get_option('fts_twitter_custom_access_token_secret') ? 'yes' : 'no' ;
+$youtubeOptions = get_option('youtube_custom_api_token') ?'yes' :'no' ;
+$instagramOptions = get_option('fts_instagram_custom_api_token') ? 'yes' : 'no' ;
 
-				jQuery('thead, tbody', '.wc_status_table').each(function(){
+$ftsDateTimeFormat = get_option('fts-date-and-time-format') ? get_option('fts-date-and-time-format') : 'no' ;
+$ftsTimezone = get_option('fts-timezone') ? get_option('fts-timezone') : 'no' ;
 
-					$this = jQuery( this );
+?>
 
-					if ( $this.is('thead') ) {
+-- Custom Token or Keys added to Options Pages
+-- You must have a custom token to use the feeds
 
-						report = report + "\n=============================================================================================\n";
-						report = report + " " + jQuery.trim( $this.text() ) + "\n";
-						report = report + "=============================================================================================\n";
+Facebook:                 <?php echo $facebookOptions 					. "\n"; ?>
+Facebook App ID:          <?php echo $facebookOptionsAppID . "\n"; ?>
+Twitter Consumer Key:     <?php echo $twitterOptions1      . "\n"; ?>
+Twitter Secret:           <?php echo $twitterOptions2      . "\n"; ?>
+Twitter Token:            <?php echo $twitterOptions3 					. "\n"; ?>
+Twitter Token Secret:     <?php echo $twitterOptions4 					. "\n"; ?>
+Instagram:                <?php echo $instagramOptions   		. "\n"; ?>
+YouTube:                  <?php echo $youtubeOptions   		. "\n"; ?>
 
-					} else {
+-- FaceBook & Twitter Date Format and Timezone
 
-						jQuery('tr', $this).each(function(){
+Date Format:              <?php echo $ftsDateTimeFormat   		. "\n"; ?>
+Timezone:                 <?php echo $ftsTimezone   		. "\n"; ?>
 
-							$this = jQuery( this );
+-- Pages or Posts with Shortcode(s).
+-- If you are using our shortcode in a widget you'll need to paste your shortcode in our support forum.
 
-							report = report + $this.find('td:eq(0)').text() + ": \t";
-							report = report + $this.find('td:eq(1)').text() + "\n";
+<?PHP echo do_shortcode("[shortcodefinderSlick find='[fts']"); ?>
 
-						});
-					}
-				});
-				report = report + '`';
-				jQuery('#fts-debug-report').val( report );
-			}
-			jQuery('#fts-debug-report').slideToggle('500', function() {
-				jQuery(this).select();
-			});
-      		return false;
-		});
-
-	</script>
+### End System Info ###</textarea>
+</form>
+<a class="fts-settings-admin-slick-logo" href="http://www.slickremix.com/support-forum/" target="_blank"></a> </div>
 <?php
 }
-?>
+
+function wpshock_search_filter( $query ) {
+    if ( $query->is_search ) {
+        $query->set( 'post_type', array('post','page') );
+    }
+    return $query;
+}
+add_filter('pre_get_posts','wpshock_search_filter');
+
+function wpb_find_shortcode_slick($atts, $content=null) { 
+ob_start();
+extract( shortcode_atts( array(
+		'find' => '',
+	), $atts ) );
+
+$string = $atts['find'];
+
+$args = array(
+	's' => $string,
+	'posts_per_page'=>100,
+	);
+
+$the_query = new WP_Query( $args );
+ 
+	$posts = $the_query->get_posts();
+	 
+foreach($posts as $post) {
+  ?>
+<<< <?php $the_query->the_post();	?>
+<?php the_permalink(); ?> >>>
+<?php remove_filter( 'the_content', 'do_shortcode', 11 ); the_content();?>
+<?php 
+}
+				
+wp_reset_postdata();
+return ob_get_clean();
+}
+add_shortcode('shortcodefinderSlick', 'wpb_find_shortcode_slick'); 
