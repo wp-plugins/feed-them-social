0694df6a62944b19384d1b75fa5f9cdad3c7e95a
slickremix <slickremix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-15 20:07:52 +0000

    fix a tag
    git-svn-id: https://plugins.svn.wordpress.org/feed-them-social/trunk@1266858 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/error-handler.php
+++ includes/error-handler.php
@@ -13,27 +13,27 @@ class fts_error_handler {
 					//ID Error
 					if(isset($feed_data->error) && $feed_data->error->code == 803){
 							if (strpos($feed_data->error->message, '(#803) Cannot query users by their username') !== false) {
-							throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.'.2 - Cannot query users by their username. <a style="color:red !important;" href="https://www.slickremix.com/docs/facebook-error-messages/#error-803-2" target="_blank">'.$solution_text.'<a/></div>');
+							throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.'.2 - Cannot query users by their username. <a style="color:red !important;" href="https://www.slickremix.com/docs/facebook-error-messages/#error-803-2" target="_blank">'.$solution_text.'</a></div>');
 						}
 						else{
-							throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.' - Facebook cannot find this ID. <a style="color:red !important;" href="https://www.slickremix.com/docs/facebook-error-messages/#error-803" target="_blank">'.$solution_text.'<a/></div>');
+							throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.' - Facebook cannot find this ID. <a style="color:red !important;" href="https://www.slickremix.com/docs/facebook-error-messages/#error-803" target="_blank">'.$solution_text.'</a></div>');
 						}
 					}
 					elseif(isset($feed_data->error) && ($feed_data->error->code == 341 || $feed_data->error->code == 4 || $feed_data->error->code == 17)){
-						throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.' - Too many calls made to Facebook. <a style="color:red !important;"href="https://www.slickremix.com/docs/facebook-error-messages/#error-rate-limiting" target="_blank">'.$solution_text.'<a/></div>');
+						throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.' - Too many calls made to Facebook. <a style="color:red !important;"href="https://www.slickremix.com/docs/facebook-error-messages/#error-rate-limiting" target="_blank">'.$solution_text.'</a></div>');
 					}
 					elseif(isset($feed_data->error) && $feed_data->error->code == 190){
-						throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.' - Error validating application. Invalid application ID. <a style="color:red !important;"href="https://www.slickremix.com/docs/facebook-error-messages/#error-invalid-app-id" target="_blank">'.$solution_text.'<a/></div>');
+						throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.' - Error validating application. Invalid application ID. <a style="color:red !important;"href="https://www.slickremix.com/docs/facebook-error-messages/#error-invalid-app-id" target="_blank">'.$solution_text.'</a></div>');
 					}
 					elseif(isset($feed_data->error) && $feed_data->error->code == 104){
-						throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.' - An access token is required to request this resource. <a style="color:red !important;"href="https://www.slickremix.com/docs/facebook-error-messages/#error-access-token-required" target="_blank">'.$solution_text.'<a/></div>');
+						throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.' - An access token is required to request this resource. <a style="color:red !important;"href="https://www.slickremix.com/docs/facebook-error-messages/#error-access-token-required" target="_blank">'.$solution_text.'</a></div>');
 					}
 					elseif(isset($feed_data->error) && $feed_data->error->code == 210){
-						throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.' This call requires a Page access token. <a style="color:red !important;"href="https://www.slickremix.com/docs/facebook-error-messages/#error-access-token-required" target="_blank">'.$solution_text.'<a/></div>');
+						throw new \Exception('<div style="clear:both; padding:15px 0;">#'.$feed_data->error->code.' This call requires a Page access token. <a style="color:red !important;"href="https://www.slickremix.com/docs/facebook-error-messages/#error-access-token-required" target="_blank">'.$solution_text.'</a></div>');
 					}
 					//Rate Limit Exceeded
 					elseif($FB_Shortcode['type'] == 'reviews' && (empty($feed_data->data) || !isset($feed_data->data))){
-						throw new \Exception('<div style="clear:both; padding:15px 0;">No Reviews Found or You may not have Admin Permissions for this page. <a style="color:red !important;"href="https://www.slickremix.com/docs/facebook-error-messages/#error-no-reviews" target="_blank">'.$solution_text.'<a/></div>');
+						throw new \Exception('<div style="clear:both; padding:15px 0;">No Reviews Found or You may not have Admin Permissions for this page. <a style="color:red !important;"href="https://www.slickremix.com/docs/facebook-error-messages/#error-no-reviews" target="_blank">'.$solution_text.'</a></div>');
 					}
 					//If Custom Exception is not needed but still error then throw ugly error.
 					elseif(isset($feed_data->error)) {
